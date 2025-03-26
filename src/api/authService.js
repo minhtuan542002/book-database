1e76a201bd8eb509924c25f9d423cc94f7f75b9c
minhtuan542002 <minhtuan542002@gmail.com>
2025-03-26 14:07:24 +1100

    Add buildspec.yml file

--- src/api/authService.js
+++ src/api/authService.js
@@ -8,14 +8,17 @@ const COGNITO_DOMAIN = process.env.REACT_APP_COGNITO_DOMAIN;
 
 export const login = async (email, password) => {
   return axios.post(`${COGNITO_DOMAIN}/oauth2/token`, 
-    new URLSearchParams({
-      grant_type: 'client_credentials',
-      client_id: COGNITO_CLIENT_ID,
-      username: email,
-      password: password,
-    }), {
+    {
+      "AuthParameters" : {
+        "USERNAME" : email,
+        "PASSWORD" : password
+      },
+      "AuthFlow" : "USER_PASSWORD_AUTH",
+      "ClientId" : `${COGNITO_CLIENT_ID}`
+    }, {
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
+      'X-Amz-Target': 'AWSCognitoIdentityProviderService.InitiateAuth',
+      'Content-Type': 'application/x-amz-json-1.1'
     }
   });
 };
